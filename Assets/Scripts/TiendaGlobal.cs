3582049d193c3f4a4f9a5f532f14b76ca0ab2752
Branxord <brandonsavanco@hotmail.com>
2019-02-03 02:28:26 -0300

    SOLUCIONADO WAXO

--- Assets/Scripts/TiendaGlobal.cs
+++ Assets/Scripts/TiendaGlobal.cs
@@ -8,13 +8,17 @@ public class TiendaGlobal : MonoBehaviour
     //INVENTARIO*************
     [HideInInspector]
     public bool cama = false;
+    [HideInInspector]
     public bool flor = false;
+    [HideInInspector]
     public bool ilumination = false;
 
     //Variables Radio.
     [HideInInspector]
-    public bool PiezaRadio1 = false;     //mover todo esto a otro script global que se relacione con la tienda
-    public bool PiezaRadio2 = false;    //hecho
+    public bool PiezaRadio1 = false;
+    [HideInInspector]                       //mover todo esto a otro script global que se relacione con la tienda
+    public bool PiezaRadio2 = false;       //hecho
+    [HideInInspector]
     public bool PiezaRadio3 = false;
 
     public static TiendaGlobal INS;
