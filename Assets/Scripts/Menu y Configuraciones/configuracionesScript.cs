db0bccd831f12007b5734af805d81757041a3c73
necronomicus <47306591+necronomicus@users.noreply.github.com>
2019-03-19 20:26:24 -0300

    asd
    ASd

---
+++ Assets/Scripts/Menu y Configuraciones/configuracionesScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using TMPro;
+public class configuracionesScript : MonoBehaviour
+{
+    public TMPro.TMP_Dropdown drop;
+    public AudioMixer audioMixer;
+    public void SetVolume(float volume)
+    {
+        Debug.Log(volume);
+        audioMixer.SetFloat("volume",volume);
+    }
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void SetFullscreen (bool isFullScreen)
+    {
+        Screen.fullScreen = isFullScreen;
+    }
+}
